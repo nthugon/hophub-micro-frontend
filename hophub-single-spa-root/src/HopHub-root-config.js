a5513d7e85d9b53cb08f8fc17e8497cf58b6ccd0
Nathan Hugon <n.hugon@f5.com>
2021-03-19 12:18:37 -0700

    feat: Registers apps.
    -registers apps with routes in root
    
    -adds required imports from running ports for systemjs
    
    -activates script for zonejs in order to run Angular
    
    -adds publicHost value to angular.json for Angular apps
    
    -adds specific ports to start scripts of apps

--- hophub-single-spa-root/src/HopHub-root-config.js
+++ hophub-single-spa-root/src/HopHub-root-config.js
@@ -1,19 +1,47 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@HopHub/hophub-header-react",
+  app: () => System.import("@HopHub/hophub-header-react"),
   activeWhen: ["/"],
 });
 
-// registerApplication({
-//   name: "@HopHub/navbar",
-//   app: () => System.import("@HopHub/navbar"),
-//   activeWhen: ["/"]
-// });
+registerApplication({
+  name: "@HopHub/hophub-footer-react",
+  app: () => System.import("@HopHub/hophub-footer-react"),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@HopHub/hophub-homepage-angular",
+  app: () => System.import("@HopHub/hophub-homepage-angular"),
+  activeWhen: [(location) => location.pathname === "/"],
+});
+
+registerApplication({
+  name: "@HopHub/hophub-aboutpage-react",
+  app: () => System.import("@HopHub/hophub-aboutpage-react"),
+  activeWhen: ["/about"],
+});
+
+registerApplication({
+  name: "@HopHub/hophub-beerspage-angular",
+  app: () => System.import("@HopHub/hophub-beerspage-angular"),
+  activeWhen: [
+    (location) =>
+      location.pathname === "/beers" || location.pathname === "/beers/",
+  ],
+});
+
+registerApplication({
+  name: "@HopHub/hophub-reviewspage-react",
+  app: () => System.import("@HopHub/hophub-reviewspage-react"),
+  activeWhen: [
+    (location) =>
+      location.pathname.includes("/beers/") &&
+      location.pathname.split("/").filter((x) => x).length > 1,
+  ],
+});
 
 start({
   urlRerouteOnly: true,
