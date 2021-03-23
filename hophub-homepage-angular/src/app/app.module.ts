3664593fdc6a70ea0398d6c84561f810dffe3310
Nathan Hugon <n.hugon@f5.com>
2021-03-23 14:54:48 -0700

    feat: Adds content to Home page.

--- hophub-homepage-angular/src/app/app.module.ts
+++ hophub-homepage-angular/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SubheadingComponent } from './components/subheading/subheading.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, SubheadingComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
