5be2fc2af08654c57ba968158c46296a5c00c0b5
Nathan Hugon <n.hugon@f5.com>
2021-03-23 15:38:45 -0700

    feat: Adds Angular routing for Home page.

--- hophub-homepage-angular/src/app/app.module.ts
+++ hophub-homepage-angular/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SubheadingComponent } from './components/subheading/subheading.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SubheadingComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SubheadingComponent,
+    EmptyRouteComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
